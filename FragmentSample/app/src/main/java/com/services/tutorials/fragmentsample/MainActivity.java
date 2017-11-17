da7da9a4a55e218ad332c3e2a24a794e65131b65
Ganesh Potnuru <ganesh.potnuru@senecaglobal.com>
2017-11-17 17:35:33 +0530

    Fragments in middle

--- FragmentSample/app/src/main/java/com/services/tutorials/fragmentsample/MainActivity.java
+++ FragmentSample/app/src/main/java/com/services/tutorials/fragmentsample/MainActivity.java
@@ -1,13 +1,43 @@
 package com.services.tutorials.fragmentsample;
 
+import android.support.annotation.IdRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonOne;
+    Button buttoTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonOne = (Button) findViewById(R.id.button);
+        buttoTwo = (Button) findViewById(R.id.button2);
+    }
+
+    public void updateFragment(View view) {
+
+        FragmentManager fm = getFragmentManager();
+
+
+        if (view == buttonOne) {
+            FragmentOne fragment1 = new FragmentOne();
+        }
+        else {
+            FragmentTwo fragment2 = new FragmentTwo();
+        }
     }
 }
